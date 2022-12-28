dafa154f16029eac7a3a04acd6374c377448eb68
Easter Maxwell <115406798+Easter-Maxwell-01@users.noreply.github.com>
2022-12-28 07:59:15 +0100

    Create image-blur.cpp

---
+++ image-blur.cpp
@@ -0,0 +1,69 @@
+/*
+       Author : Easter Maxwell
+       Date : 28th December, 2022
+
+       Program : Blur image using Gaussian blur
+*/
+
+#include <iostream>
+#include <windows.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat gray;
+    Mat blur;
+    Mat image;
+    Mat edges;
+    const int ratio = 1;
+    int low_threshold = 50;
+
+    /*higher threshold -> more blur*/
+
+    /*image source*/
+    image = imread("C:\\Users\\MAX EASTER\\Downloads\\Tumblr.png");
+
+    if (image.empty())
+    {
+        cerr << endl
+             << "\a`--> Error : loading image!"
+             << endl;
+
+        Sleep(2000);
+
+        return 1;
+    }
+
+    
+    /*convert image color to grayscale*/
+    cvtColor(image, gray, COLOR_BGR2GRAY);
+
+    /*
+      lower number -> less blur
+      larger number -> more blur
+      note: must be an odd number
+    */
+    Size kernel_size(5, 5);
+
+    /*GBlur(inputImage, outputImage, size, 0)*/
+    GaussianBlur(gray, blur, kernel_size, 0);
+
+    /*detect edges in the image using canny edge detector
+      canny(grayscale image, detector's output, lowthreshold, lowthreshold * 1)
+    */
+    Canny(blur, edges, low_threshold, low_threshold * ratio);
+
+    /*diplay images*/
+    imshow("original", image);
+    imshow("Processed", edges);
+
+    waitKey();
+
+    destroyAllWindows();
+
+    return 0;
+
+}
