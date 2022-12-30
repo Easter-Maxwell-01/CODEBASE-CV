71deab6faf785e9ed64dd7a99cb95c0b1e9c4972
Easter Maxwell <115406798+Easter-Maxwell-01@users.noreply.github.com>
2022-12-30 22:44:01 +0100

    Create file_structure.cpp

---
+++ Asst/file_structure.cpp
@@ -0,0 +1,27 @@
+
+ //                      file-tree structure of the Ghost-2006
+/*                                                                                                         
+  __CORE  [ C++, python ]                                   ___GUI / UX [ python ]                         
+    |                                                          |                                           
+    |____human count___________________________________________|____Command Library                         
+    |___object count__________________________________________________|                                     
+    |    |____________________________________________________________|______Graphics / UX                 
+    |    |____alert system___________________________________________________|                            
+    |        |                                                               |______Menu box           
+    |        |____image capture                                                                            
+    |        |____video capture_______                                                                      
+    |                                 |                                                                     
+    |____human detection              |                     ____M/L [ python ]                              
+    |___object detection              |_________________________|                                           
+        |   |                         |_________________________|____alert system                          
+        |   |____image processing ____]______________________________|                                     
+        |   |____video processing ____]______________________________|____human detection                   
+        |                                                             |___object detection          
+        |                                                                 |                                 
+        |____facial recognition___________________________________________|____object recognition          
+        |____object recognition______________________________________________|___facial recognition         
+               |                                                               |__                          
+               |___pattern recognition____________________________________________|___pattern recognition 
+
+*/
+
