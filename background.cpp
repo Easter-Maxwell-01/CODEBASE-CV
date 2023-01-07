56a663cf97d662b83f501aa04281d04a1e54b3ea
Easter Maxwell <115406798+Easter-Maxwell-01@users.noreply.github.com>
2023-01-08 00:10:03 +0100

    Update background.cpp

--- background.cpp
+++ background.cpp
@@ -1,9 +1,9 @@
 /*
        Author : Easter Maxwell
        Date : 28th December, 2022
-       
+
        Program : background subtractor
-       
+
        Note : use the esc key to exit the program
 */
 
@@ -19,6 +19,10 @@ int main()
     char key;
     Mat frame;
     Mat foreground;
+    double frame_width;
+    double frame_height;
+
+    Ptr<BackgroundSubtractor> motion_detector;
 
     /*open default camera*/
     VideoCapture camera(0);
@@ -26,17 +30,26 @@ int main()
     /*didn't succeed*/
     if (!camera.isOpened())
     {
-        cerr << " `--> Error : opening camera" << endl;
+        cerr << "\n`--> Error : opening camera" << endl;
 
         Sleep(2000);
 
         return 1;
     }
 
-    Ptr<BackgroundSubtractor> motion_detector;
-
     /*set up the motion detector*/
     motion_detector = createBackgroundSubtractorMOG2();
+
+    /*get the width and height of the frame*/
+    frame_width = camera.get(cv::CAP_PROP_FRAME_WIDTH);
+    frame_height = camera.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+    /*  
+        Define the codec and create a VideoWriter object. 
+        The video output is stored in 'video_file.mp4v' file.
+    */
+    VideoWriter video("video_file.mp4v", cv::VideoWriter::fourcc
+    ('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height));
 
     while (true)
     {
@@ -59,13 +72,16 @@ int main()
         imshow("Surveillance", frame);
         imshow("Foreground", foreground);
 
+        /*save the video file*/
+        video.write(foreground);
+
         /*check for user input*/
         key = (char)waitKey(30);
 
         /*escape key*/
         if (key == 27)
         {
-            cout << "`--> Program terminated!" << endl;
+            cout << "\n`--> Program terminated!" << endl;
 
             Sleep(2000);
 
